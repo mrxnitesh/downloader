2dd76639c28a68d4d425b627e5082886d6c665a4
Mr Nitesh <mrnitesh.email+github@gmail.com>
2024-05-06 22:30:38 +0530

    script.js added

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-// Add your JavaScript code here
 document.getElementById('downloadForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var url = document.getElementById('videoUrl').value;
-    fetch('/download', {
+    fetch('https://downloader-backend-kej3mh2jr-mr-niteshs-projects.vercel.app/download', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({ url: url })
     })
-    .then(response => response.text())
-    .then(message => {
-        document.getElementById('message').textContent = message;
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('message').textContent = data.message;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('message').textContent = 'An error occurred. Please try again later.';
     });
 });
